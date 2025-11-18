dd1e1ad1b6cb95ddd4b0ec972d97ee8e7c841eed
sumitverma32828-netizen <sumitverma32828@gmail.com>
2025-11-18 18:48:57 +0530

    Revert "deleted multiple files"
    This reverts commit 6572e29a40ca225f237cda70896e127184748cb8.

---
+++ SIMPLEINTREST.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    float A,B,C;
+    printf("enter value of A, B, C,:");
+    scanf("%f,%f,%f,", &A, &B, &C);
+   float SI = (A*B*C)/100;
+printf("%f",SI);
+return  0;
+}
