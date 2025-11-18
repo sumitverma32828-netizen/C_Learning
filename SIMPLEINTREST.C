6572e29a40ca225f237cda70896e127184748cb8
sumitverma32828-netizen <sumitverma32828@gmail.com>
2025-11-18 13:11:28 +0530

    deleted multiple files

--- SIMPLEINTREST.C
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    float A,B,C;
-    printf("enter value of A, B, C,:");
-    scanf("%f,%f,%f,", &A, &B, &C);
-   float SI = (A*B*C)/100;
-printf("%f",SI);
-return  0;
-}
